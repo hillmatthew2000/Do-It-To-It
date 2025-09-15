41a99b7c19b1ece7caf76986bada87ddf4edc309
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-09-15 15:14:07 -0500

    Add console.bat file for console app running

---
+++ do-it-to-it/console.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Starting Do-It-To-It Console Mode...
+echo.
+"C:\Program Files\Eclipse Adoptium\jdk-21.0.8.9-hotspot\bin\java.exe" -jar "C:\Do-It-To-It\do-it-to-it\target\do-it-to-it-0.0.1-SNAPSHOT.jar"
+pause
